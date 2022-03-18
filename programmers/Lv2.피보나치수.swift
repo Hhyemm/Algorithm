1f3a5a3d0ca42696a1d21ec214403de773be0e55
Hhyemm <hyem0127@gmail.com>
2022-03-18 09:54:45 +0900

    [programmers] Lv.2_피보나치수

---
+++ programmers/Lv2.피보나치수.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var d = Array(repeating: 0, count: n+1)
+    d[1] = 1
+    for i in 2...n {
+        d[i] = d[i-1] + d[i-2] % 1234567
+    }
+    return d[n]
+}
