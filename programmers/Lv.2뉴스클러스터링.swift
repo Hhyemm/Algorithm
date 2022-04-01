999292cd8d63ee1b60bc0015ba737fe903ee1fea
Hhyemm <hyem0127@gmail.com>
2022-04-01 15:44:21 +0900

    [programmers] Lv.2_뉴스클러스터링

--- programmers/Lv.2뉴스클러스터링.swift
+++ programmers/Lv.2뉴스클러스터링.swift
@@ -2,6 +2,54 @@
 import Foundation
 
 func solution(_ str1:String, _ str2:String) -> Int {
+    let str1_ = str1.map{String($0).uppercased()}
+    let str2_ = str2.map{String($0).uppercased()}
+    var str1Arr = [String]()
+    var str2Arr = [String]()
+    var intersection = [String]()
+    var union = [String]()
+    
+    for i in 1..<str1_.count {
+        if isLetter(str1_[i-1]) && isLetter(str1_[i]) {
+            str1Arr.append("\(str1_[i-1])\(str1_[i])")
+        }
+    }
+    
+    for i in 1..<str2_.count {
+        if isLetter(str2_[i-1]) && isLetter(str2_[i]) {
+            let s2 = "\(str2_[i-1])\(str2_[i])"
+            str2Arr.append(s2)
+            if str1Arr.contains(s2) {
+                intersection.append(s2)
+                str1Arr.remove(at: str1Arr.firstIndex(of: s2)!)
+                str2Arr.remove(at: str2Arr.firstIndex(of: s2)!)
+            }
+        }
+    }
+    union = str1Arr + str2Arr + intersection
+    return union.count == 0 ? 65536 : intersection.count == 0 ? 0 : Int((Double(intersection.count)/Double(union.count))*65536)
+}
+
+func isLetter(_ str: String) -> Bool {
+    for s in str {
+        if !s.isLetter {
+            return false
+        }
+    }
+    return true
+}
+
+/*
+print(solution("FRANCE", "french"))
+print(solution("handshake", "shake hands"))
+print(solution("aa1+aa2", "AAAA12"))
+print(solution("E=M*C^2", "e=m*c^2"))
+print(solution("A+C", "DEF"))
+*/
+
+
+/*
+func solution(_ str1:String, _ str2:String) -> Int {
     let str1_ = multiplex(str1)
     let str2_ = multiplex(str2)
     var intersection = [Int]()
@@ -38,11 +86,5 @@ func multiplex(_ str: String) -> [String:Int] {
     }
     return dic
 }
-
-/*
-print(solution("FRANCE", "french"))
-print(solution("handshake", "shake hands"))
-print(solution("aa1+aa2", "AAAA12"))
-print(solution("E=M*C^2", "e=m*c^2"))
-print(solution("A+C", "DEF"))
 */
+
