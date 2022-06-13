5feb7f3550a0dfd377a5001d1b24d7c398a3f0ec
Hhyemm <hyem0127@gmail.com>
2022-06-13 23:02:31 +0900

    [programmers] Lv.2_멀리뛰기

---
+++ programmers/Lv.2멀리뛰기.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var dp = Array(repeating: 0, count: 2001)
+    dp[1] = 1
+    dp[2] = 2
+    
+    if n >= 3 {
+        for i in 3...n {
+            dp[i] = dp[i-2] + dp[i-1] % 1234567
+        }
+    }
+    return dp[n]
+}
+
