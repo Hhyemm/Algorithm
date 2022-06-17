b3d75bac4b80c0d182955b9b782b0fdc3da42a38
Hhyemm <hyem0127@gmail.com>
2022-06-17 21:06:31 +0900

    [BOJ] 2579_계단오르기

---
+++ BOJ/2579계단오르기.swift
@@ -0,0 +1,23 @@
+
+import Foundation
+
+func solution2579() {
+    let n = Int(readLine()!)!
+    var arr = [Int]()
+    for _ in 0..<n {
+        arr.append(Int(readLine()!)!)
+    }
+    var dp = Array(repeating: 0, count: n+1)
+    dp[1] = arr[0]
+    if n > 1 {
+        dp[2] = arr[0] + arr[1]
+    }
+
+    if n > 2 {
+        for i in 3...n {
+            dp[i] = max(dp[i-2] + arr[i-1], dp[i-3] + arr[i-2] + arr[i-1])
+        }
+    }
+    print(dp[n])
+
+}
