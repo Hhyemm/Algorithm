6a3a8824a41c01fa0e036d9fdbbc8274c542811f
Hhyemm <hyem0127@gmail.com>
2022-06-20 16:31:33 +0900

    [BOJ] 11722_가장긴감소하는부분수열

---
+++ BOJ/11722가장긴감소하는부분수열.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+
+func solution11722() {
+    let n = Int(readLine()!)!
+    let arr = readLine()!.split(separator: " ").map{Int($0)!}
+    var dp = Array(repeating: 1, count: n+1)
+
+    for i in 1...n {
+        for j in 1...i {
+            if arr[i-1] < arr[j-1] {
+                dp[i] = max(dp[i], dp[j]+1)
+            }
+        }
+    }
+    print(dp.max()!)
+
+}
